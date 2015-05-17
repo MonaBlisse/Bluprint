2c599a06d23be0575324459e473b2914d573dcaa
Rae Capri <raecapri@gmail.com>
2015-05-17 17:22:38 -0400

    Added BrowserSync capabilities to watch live changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,32 @@ var gulp = require('gulp'),
 		concat = require('gulp-concat'),
 		rename = require('gulp-rename'),
 		sass = require('gulp-sass'),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		browserSync = require('browser-sync').create(),
+		reload = browserSync;
+
+//BrowserSync for starting server
+gulp.task('browserSync', function () {
+	var files = [
+	'./css/style.css',
+	'./*.php'
+	];
+
+	browserSync.init(files, {
+		proxy: 'wpdev.com',
+		notify: false
+	});
+});
 
 gulp.task('sass', function () {
 	gulp.src('sass/style.scss')
-		.pipe(sass({
-			style: 'compressed'
-		}))
+		.pipe(sass())
 		.pipe(gulp.dest('css'))
-		.pipe(concat('style.css'))
-		.pipe(gulp.dest('css'))
-		.pipe(uglify());
+		.pipe(browserSync.reload({stream:true, once: true}));
 });
 
 gulp.task('watch', function () {
 	gulp.watch('sass/style.scss', ['sass']);
 });
 
-gulp.task('default', ['watch', 'sass']);
+gulp.task('default', ['watch', 'sass', 'browserSync']);
