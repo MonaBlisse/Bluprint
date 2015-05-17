7d9b3243c15a907fc11b7c88f5273dae91a0e4d2
Rae Capri <raecapri@gmail.com>
2015-05-17 17:28:29 -0400

    Added the optional show/hide Admin Bar

--- functions.php
+++ functions.php
@@ -141,3 +141,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Custom Settings
+ */
+add_filter('show_admin_bar', '__return_false');
