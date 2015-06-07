c41f2178ebf6def54470e400179ab98cfa22acfa
Rae Capri <raecapri@gmail.com>
2015-06-07 09:50:50 -0400

    Used a different method that is easier for SASS and CSS injection with Browser-Sync options

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,48 @@ var gulp = require('gulp'),
 		browserSync = require('browser-sync').create(),
 		reload = browserSync;
 
-//BrowserSync for starting server
-gulp.task('browserSync', function () {
-	//files to keep a close watch on
-	var files = [
-	'./css/style.css',
-	'./sass/**/*.scss',
-	'./*.php'
-	];
+// Config for various settings
+var config = {
+	bowerDir: 'bower_components'
+}
 
-	//Initializing BrowserSync with virtual host
-	browserSync.init(files, {
-		proxy: '<add vhost/domain here>',
-		notify: false
+gulp.task('bower', function () {
+	return bower()
+		.pipe(gulp.dest(config.bowerDir))
+});
+
+// Moving FontAwesome Icons to New Folder
+gulp.task('fa-icons', function() { 
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('src/fonts')); 
+});
+
+// Moving FontAwesome CSS file to New Folder
+gulp.task('fa-styles', ['fa-icons'], function () {
+	return gulp.src(config.bowerDir + '/fontawesome/css/**.*') 
+        .pipe(gulp.dest('src/fonts/css')); 
+});
+
+//BrowserSync for starting static server
+gulp.task('browserSync', ['sass'], function () {
+	browserSync.init({
+		server: "./"
+		// or use proxy: "wpdev.com"
 	});
+
+	gulp.watch('sass/*.scss', ['sass']);
+	gulp.watch('*.php').on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
 	gulp.src('sass/**/*.scss')
-		.pipe(sass())
+		.pipe(sass({
+			loadPath: [
+				config.bowerDir + '/fontawesome/scss'
+			]
+		}))
 		.pipe(gulp.dest('css'))
-		.pipe(browserSync.reload({stream:true, once: true}));
+		.pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-	gulp.watch('sass/**/*.scss', ['sass']);
-});
-
-gulp.task('default', ['watch', 'sass', 'browserSync']);
+gulp.task('default', ['browserSync']);
