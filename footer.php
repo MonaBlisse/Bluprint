9c5a41ca3d3e8ad9cbd8d0fec61457fc0573ce50
Rae Capri <raecapri@gmail.com>
2015-06-07 09:45:23 -0400

    Removed Browser-Sync script tag, as no longer needed with new Brower-Sync update

--- footer.php
+++ footer.php
@@ -20,8 +20,5 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.7.1.js'><\/script>".replace("HOST", location.hostname));
-//]]></script>
 </body>
 </html>
